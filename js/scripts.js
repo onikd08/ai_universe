73e80c92ef9d639c55931fe7d1b6879d824a4c1d
Anik Das <onikd08@gmail.com>
2023-10-12 10:56:32 +0300

    Add modal and show details

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ const displayData = (data) => {
  <div class="card-body">
     <h2 class="card-title"> Features </h2>
    <ol class="list-decimal list-inside ">
-    ${displayFeatures(features)}
+    ${displayListItems(features)}
    </ol>
    <hr class="h-px my-4 bg-gray-300 border-0 ">
 
@@ -33,7 +33,7 @@ const displayData = (data) => {
         <h2 class="card-title">${name}</h2>
         <p>${published_in} </p>
     </div>
-    <button class="btn btn-primary">Buy Now</button>
+    <button class="btn btn-info btn-outline" onclick="handleShowDetails('${id}')">Show Details</button>
   </div>
  </div>
   `;
@@ -42,10 +42,58 @@ const displayData = (data) => {
   });
 };
 
-const displayFeatures = (features) => {
+const displayListItems = (items) => {
   let element = "";
-  features.forEach((feature) => {
-    element += `<li>${feature} </li>`;
+  items.forEach((item) => {
+    element += `<li>${item} </li>`;
   });
   return element;
+};
+
+const handleShowDetails = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  );
+  const data = await res.json();
+  displayDetails(data.data);
+  show_details_modal.showModal();
+};
+
+const displayDetails = (data) => {
+  const { description, image_link, features, integrations } = data;
+  const modalContentContainer = document.getElementById(
+    "modal-content-container"
+  );
+  let featureList = [];
+  for (const item in features) {
+    const { feature_name } = features[item];
+    featureList.push(feature_name);
+  }
+  modalContentContainer.textContent = "";
+  const divContent = document.createElement("div");
+  const divImage = document.createElement("div");
+  divContent.innerHTML = `
+    <p class="font-semibold">${description} </p>
+    <div class="flex justify-between pt-4">
+    <div>
+    <p class="font-semibold">Features </p>
+    <ul class="list-disc list-inside ">
+    ${displayListItems(featureList)}
+   </ul>
+    </div>
+   <div>
+   <p class="font-semibold">Integrations </p>
+   <ul class="list-disc list-inside ">
+    ${displayListItems(integrations)}
+   </ul>
+   </div>
+    </div>
+  `;
+  divImage.innerHTML = `
+    <img
+        src=${image_link[0]} 
+    />
+  `;
+  modalContentContainer.appendChild(divContent);
+  modalContentContainer.appendChild(divImage);
 };
